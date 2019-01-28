f11d719e09ed8e3cccc00d1bd3345f31ad10eb45
maninder <mkaur@innow8apps.com>
2019-01-28 12:36:46 +0530

    - qrscan issue on ios fixed

--- src/common/QRScanner/view.js
+++ src/common/QRScanner/view.js
@@ -3,12 +3,9 @@ import React, { Component } from 'react';
 
 import { View, StyleSheet, StatusBar, Alert } from 'react-native';
 import Web3 from 'web3';
-// import { DEVICE_HEIGHT } from '../../../common/constants';
-// import arrowLeftButton from '../../../images/arrowLeft_White.png';
-// import Header from '../../../general/header/index';
+
 import QRCodeScanner from './index';
 import TitleHeader from '../TitleHeader';
-// const deviceHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   centerText: {
@@ -34,14 +31,13 @@ const styles = StyleSheet.create({
 });
 
 export default class ScanScreen extends Component {
-  // 0x3E22efd6FA7Af208FAF1ECF2c356D7611bB06F30
   onSuccess(e) {
     const { navigation } = this.props;
     const successFunc = navigation.getParam('onScanSuccess', null);
     const { data } = e;
     if (data) {
       const dataStr = data.trim();
-      const isValid = Web3.utils.isAddress(dataStr);
+      const isValid = Web3.prototype.isAddress(dataStr);
       if (!isValid) {
         Alert.alert('Error', 'Please scan a valid QR code.', [
           {
@@ -69,7 +65,7 @@ export default class ScanScreen extends Component {
     const { navigation } = this.props;
     return (
       <View style={styles.mainContainerStyle}>
-        <StatusBar barStyle="light-content" />
+        <StatusBar barStyle="dark-content" />
 
         <TitleHeader
           iconName="keyboard-arrow-left"
